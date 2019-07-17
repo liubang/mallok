9ec498a0cf653734c730304662b4d89032d0fcc3
liubang <it.liubang@gmail.com>
2019-07-17 15:52:54 +0800

    测试

--- test.c
+++ test.c
@@ -12,5 +12,17 @@ int main(int argc, char *argv[]) {
     s->age = 26;
     printf("s:{name:%s, age:%d}\n", s->name, s->age);
     free(s);
+
+    void *arr = calloc(2, sizeof(struct student));
+    
+    ((struct student *)(arr))->name = "liubang 1";
+    ((struct student *)(arr))->age = 26;
+
+    ((struct student *)(arr + sizeof(struct student)))->name = "liubang 2";
+    ((struct student *)(arr + sizeof(struct student)))->age = 26;
+
+    arr = realloc(arr, sizeof(struct student) * 3);
+
+    free(arr);
     return 0;
 }
